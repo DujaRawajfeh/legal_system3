402575f43681cc08dc6ed88dbc525d3a812d97a6
ezzdawoud <azzdawud15@gmail.com>
2025-12-10 19:23:20 +0300

    Refactor security settings link in judge dashboard for direct window opening instead of route navigation

--- resources/views/clerk_dashboard/judge.blade.php
+++ resources/views/clerk_dashboard/judge.blade.php
@@ -192,7 +192,7 @@ button:hover, .btn:hover {
   <div class="left-section">
     <span class="judge-name">القاضي/ {{ $judge->full_name }}</span>
     <ul class="nav-links">
-      <li><a href="{{ route('2fa.setup') }}" class="security-link">اعدادات الحماية</a></li>
+      <li><a href="#" class="security-link" onclick="openWindow('securitysettings')">اعدادات الحماية</a></li>
     </ul>
     <ul class="nav-tabs">
       <li><a href="#" class="active" onclick="showTab('casesTab', this)">الدعاوى</a></li>
