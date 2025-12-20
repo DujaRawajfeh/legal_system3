eac44a3c29f1719b914ff9a25fc60e1e7fb83b44
ezzdawoud <azzdawud15@gmail.com>
2025-12-20 20:10:22 +0300

    Add Axios script to typist page for enhanced functionality

--- resources/views/clerk_dashboard/typist.blade.php
+++ resources/views/clerk_dashboard/typist.blade.php
@@ -8,6 +8,7 @@
 <title>صفحة الطابعة</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap');
 
