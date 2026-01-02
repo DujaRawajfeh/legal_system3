27734f28950b84073ae359a87ace499ad6b045fc
e.dawoud <e.dawoud@hawsabah.sa>
2026-01-02 20:50:05 +0300

    Add session status column to request details table in entry search popup

--- resources/views/components/entry-search-bar.blade.php
+++ resources/views/components/entry-search-bar.blade.php
@@ -139,6 +139,7 @@ async function loadEntrySearchRequestDetails(requestNumber) {
                 <tr>
                     <td>${s.date ?? '-'}</td>
                     <td>${s.time ?? '-'}</td>
+                    <td>${s.session_status ?? '-'}</td>
                     <td>${s.goal ?? '-'}</td>
                     <td>${s.reason ?? '-'}</td>
                 </tr>`;
@@ -170,10 +171,11 @@ async function loadEntrySearchRequestDetails(requestNumber) {
                         <th>تاريخ الجلسة</th>
                         <th>وقت الجلسة</th>
                         <th>حالة الجلسة</th>
+                        <th>غاية الجلسة</th>
                         <th>سبب الجلسة</th>
                     </tr>
                 </thead>
-                <tbody>${sessionsHTML || '<tr><td colspan="4" class="text-center">لا توجد جلسات</td></tr>'}</tbody>
+                <tbody>${sessionsHTML || '<tr><td colspan="5" class="text-center">لا توجد جلسات</td></tr>'}</tbody>
             </table>
 
             <h6 class="mt-4">الأطراف</h6>
